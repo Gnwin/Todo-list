a3a9799c5a09177c9b4b4b18048b7a687383d1f9
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-28 10:37:39 +0100

    fix linter errors dangle-comma

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   mode: 'development',
