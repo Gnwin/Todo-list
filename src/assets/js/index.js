fe45776d0f90137cef2f703b2a86ce80a6c68195
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-29 21:15:34 +0100

    create todo item

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,3 +2,10 @@
 import '../stylesheets/reset.css';
 import '../stylesheets/style.css';
 
+class todo {
+  constructor (description, index){
+    this.description = description;
+    this.completed = false;
+    this.index = index
+  }
+}
