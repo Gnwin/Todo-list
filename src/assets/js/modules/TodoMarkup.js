c0009a2a7cab16ae600f45cfe91262376bc82e7f
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-30 11:44:05 +0100

    keep main branch clean

--- src/assets/js/modules/TodoMarkup.js
+++
@@ -1,7 +0,0 @@
-const todomarkup = (todo) => `<div class="space">
-<input class="input" type="checkbox" name="" value="" id="">
-<input class="list-title" type="text" name="" value=${todo.description} id="${todo.index}"/>
-<img src="https://img.icons8.com/material-rounded/24/000000/menu-2.png" class="threedots"/>
-</div>`;
-
-export default todomarkup;
